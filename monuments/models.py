aad93bf8932a900d63dcf6b01ad3dd802bcf9fa5
PetitGrigri <fgriselles@hotmail.fr>
2017-06-24 11:16:29 +0200

    Initialisation du modèle sans relation

--- monuments/models.py
+++ monuments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+# Utilisateur
+class User(models.Model):
+    first_name = models.CharField(max_length=200, blank=False)
+    last_name = models.CharField(max_length=200, blank=False)
+
+class Note(models.Model):
+    note = models.TextField(blank=False)
+
+class Monument(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+
+
+class Address(models.Model):
+    address_1 = models.CharField(max_length=200, blank=False)
+    address_2 = models.CharField(max_length=200)
+
+class City(models.Model):
+    zip_code = models.IntegerField(blank=False)
+    city_name = models.CharField(max_length=200, blank=False)
+
+
