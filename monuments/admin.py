65ce3cff84418a0107c337944ed8d74e5863f51d
fisamada39 <asifadam93@hotmail.fr>
2017-06-25 20:22:53 +0200

    admin page activated, username & password @ monuments/admin.py

--- monuments/admin.py
+++ monuments/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+# Username : admin
+# Password : esgi4moc1
+
+admin.site.register(Person)
+admin.site.register(City)
+admin.site.register(Address)
+admin.site.register(Monument)
+admin.site.register(Note)
 
