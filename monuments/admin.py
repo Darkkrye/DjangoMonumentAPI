1d358e16ed497b2368fd7d89c7bab5a4c32a6c2b
fisamada39 <asifadam93@hotmail.fr>
2017-06-25 20:28:51 +0200

    mini edits

--- monuments/admin.py
+++ monuments/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 # Register your models here.
 
+# Url : http://127.0.0.1:8000/admin/
 # Username : admin
 # Password : esgi4moc1
 
--- monuments/models.py
+++ monuments/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # L'utilisateur
 class Person(User):
     class Meta:
         proxy = True
         ordering = ('first_name', 'last_name')
+
 
 # Ville
 class City(models.Model):
@@ -17,7 +19,8 @@ class City(models.Model):
 class Address(models.Model):
     address_1 = models.CharField(max_length=200, blank=False)
     address_2 = models.CharField(max_length=200, blank=True, null=True)
-    city = models.ForeignKey(City, blank=True, null=True, on_delete=models.CASCADE) # si on supprime une City, on détruit l'adresse
+    city = models.ForeignKey(City, blank=True, null=True,
+                             on_delete=models.CASCADE)  # si on supprime une City, on détruit l'adresse
 
 
 # Le monument
@@ -30,10 +33,7 @@ class Monument(models.Model):
 # Note est la table intermédiaire pour la relation Many to Many entre les utilisateurs et les monuments
 class Note(models.Model):
     note = models.TextField(blank=False)
-    user = models.ForeignKey(Person, blank=False, null=True, on_delete=models.SET_NULL) #si on détruit l'utilisateur, set la FK user à null
-    monument = models.ForeignKey(Monument, blank=False, null=True, on_delete=models.CASCADE) # si on détruit le monument on supprime la note
-
-
-
-
-
+    user = models.ForeignKey(Person, blank=False, null=True,
+                             on_delete=models.SET_NULL)  # si on détruit l'utilisateur, set la FK user à null
+    monument = models.ForeignKey(Monument, blank=False, null=True,
+                                 on_delete=models.CASCADE)  # si on détruit le monument on supprime la note
